b28d3e12f522a5ec05a91a455131aba8ac5013fa
Rafael Alcantara <rafaelcampalc@gmail.com>
2025-03-05 15:57:34 -0600

    Included package function dependencies

---
+++ R/BARDDT-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom doParallel registerDoParallel
+#' @importFrom foreach %do%
+#' @importFrom foreach %dopar%
+#' @importFrom foreach foreach
+#' @importFrom parallel makeCluster
+#' @importFrom parallel stopCluster
+#' @importFrom stochtree bart
+## usethis namespace: end
+NULL
